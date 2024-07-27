f4800d5e383c50236bb9b897cd638ba3ceed1f0f
tajemouti <tajemouti@gmail.com>
2024-07-27 15:24:54 +0100

    Update the home component logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    HttpClientModule,
+    NgFor
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  trendingMovies: any;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() :void {
+    this.getTrendingMovies();
+  }
+
+  getTrendingMovies() {
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe( (movies) => {
+      this.trendingMovies = movies;
+      console.log(this.trendingMovies);
+    });
+  }
 }
