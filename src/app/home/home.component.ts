1fbda27593f4317042328f035155fd9e38dd6446
tajemouti <tajemouti@gmail.com>
2024-07-29 11:26:19 +0100

    Define goToMovie method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from "../header/header.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgFor, NgIf } from '@angular/common';
 import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -19,21 +20,40 @@ import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomeComponent {
   trendingMovies: any;
+  popularMovies: any;
+  theatreMovies: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() :void {
     this.getTrendingMovies();
+    this.getPopularMovies();
+    this.getTheatreMovies();
   }
 
   getTrendingMovies() {
     this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe( (movies) => {
       this.trendingMovies = movies;
-      console.log(this.trendingMovies);
+    });
+  }
+
+  getTheatreMovies() {
+    this.http.get('http://localhost:4200/assets/data/theatre-movies.json').subscribe( (movies) => {
+      this.theatreMovies = movies;
+    });
+  }
+
+  getPopularMovies() {
+    this.http.get('http://localhost:4200/assets/data/popular-movies.json').subscribe( (movies) => {
+      this.popularMovies = movies;
     });
   }
 
   ariaValueText(current: number, max: number) {
 		return `${current} out of ${max} hearts`;
 	}
+
+  goToMovie(type: string, id: string) {
+    this.router.navigate(['movie', type, id]);
+  }
 }
